01260b325051ea1e107798a3d021487b1987bb7e
Marian Ciortea <marian.ciortea@yahoo.com>
2019-06-14 21:00:34 +0300

    Include wwwroot folder to source control.

---
+++ src/Web/wwwroot/js/payment.js
@@ -0,0 +1,37 @@
+﻿var Payment = function () {
+    var init = function () {
+        var form = document.querySelector('#PaymentForm');
+        var nonceInput = document.querySelector('#PaymentMethodNonce');
+        var clientToken = $('#ClientToken').val();
+
+        braintree.dropin.create({
+            authorization: clientToken,
+            container: '#Dropin'
+        }, function (error, instance) {
+                if (error && error !== null) {
+                    console.log(error);
+                    return;
+                }
+
+                form.style.display = 'block';
+
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault();
+
+                    instance.requestPaymentMethod(function (error, payload) {
+                        if (error && error !== null) {
+                            console.log(error);
+                            return;
+                        }
+
+                        nonceInput.value = payload.nonce;
+                        form.submit();
+                    });
+                });
+        });
+    };
+
+    return {
+        init: init
+    };
+}();
